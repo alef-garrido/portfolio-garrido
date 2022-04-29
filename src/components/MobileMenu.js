0c9a45ecd44e8bd2bce0be66f617de99f187ae1c
alef-garrido <oa.g11@icloud.com>
2022-04-28 22:24:24 -0500

    Fix navbar responsive layout

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types';
 import { AiOutlineCloseSquare } from 'react-icons/ai';
+import logo from '../assets/logo.svg';
 
 function MobileMenu(props) {
   const { clickHandler } = props;
   return (
     <div className="mobile--menu">
-      <button
-        type="button"
-        className="menu--mobile-closeBttn"
-        onClick={clickHandler}
-      >
-        <AiOutlineCloseSquare />
-      </button>
+      <div className="mobile--header">
+        <img alt="" src={logo} />
+        <button
+          type="button"
+          className="menu--mobile-closeBttn"
+          onClick={clickHandler}
+        >
+          <AiOutlineCloseSquare />
+        </button>
+      </div>
       <ul>
         <li>
           <a href="#home">Home</a>
@@ -21,6 +25,9 @@ function MobileMenu(props) {
         </li>
         <li>
           <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#footer">Connect with Me</a>
         </li>
       </ul>
     </div>
