dbda532658e09342d6ecf71ab6fe94da8cfcd78e
alef-garrido <oa.g11@icloud.com>
2022-04-30 20:52:49 -0500

    Fix mobile layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,10 +4,12 @@ import cover1 from '../assets/thumbnails/thumbnail-1.jpg';
 function Projects() {
   return (
     <section className="projects--container">
-      <div className="project-thumbnail-full">
-        <div className="project-miniature">
-          <img alt="" src={cover1} className="cover--display" />
-          <p />
+      <div className="project-thumbnail-full" />
+      <div className="project-miniature">
+        <img alt="" src={cover1} className="cover--display" />
+        <div className="miniature--title">
+          <h5>Title</h5>
+          <p>This is a paragraph wit info</p>
         </div>
       </div>
     </section>
